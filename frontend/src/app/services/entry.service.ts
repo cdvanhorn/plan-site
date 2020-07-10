4f076b5878db47569c87869de791cff7e238c633
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-07-10 15:08:01 -0400

    I can load json from the filesystem

--- frontend/src/app/services/entry.service.ts
+++ frontend/src/app/services/entry.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {ENTRIES} from '../models/mock/entries';
 import {Entry} from '../models/entry';
 import { Observable, of } from 'rxjs';
+import * as planData from '../../data/plan.json';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,8 @@ export class EntryService {
   constructor() { }
 
   getEntries(): Observable<Entry[]> {
+    //we're going to build an array of entries from some json
+    console.log(planData);
     return of(ENTRIES);
   }
 }
